f046ef7b18c5a19ec1e911843ae32cfcc8d84753
schroe0690 <schroedinger.gs@gmail.com>
2025-05-06 21:29:31 +0900

    Update nvchad config 2025-05-06 21:29:31

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,20 +5,28 @@
 ---@type ChadrcConfig
 local M = {}
 
+-- テーマの設定
 M.base46 = {
 	theme = "scaryforest",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+-- ファイルツリーの設定
+M.nvimtree = {
+  view = {
+    width = 15,  -- ファイルツリーの幅を30に設定
+    side = "left",  -- 左側に表示
+  },
+  git = {
+    enable = true,  -- Gitの状態を表示
+  },
+  renderer = {
+    highlight_git = true,  -- Gitの状態をハイライト
+    icons = {
+      show = {
+        git = true,  -- Gitアイコンを表示
+      },
+    },
+  },
+}
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,16 +13,28 @@ return {
     end,
   },
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  -- 透過用のプラグイン
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    config = function()
+      require("transparent").setup({
+        groups = {
+          'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+          'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+          'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+          'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+          'EndOfBuffer',
+        },
+        extra_groups = {
+          "NormalFloat", "NvimTreeNormal", "NvimTreeEndOfBuffer", "TelescopeNormal",
+          "TelescopeBorder", "WhichKeyFloat", "FloatBorder",
+        },
+        exclude_groups = {},
+      })
+      -- デフォルトで透過を有効化
+      vim.cmd("TransparentEnable")
+    end,
+  },
+  
 }
