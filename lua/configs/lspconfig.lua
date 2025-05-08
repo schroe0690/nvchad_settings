9f0abe8860334fa79a9fd881eaf98a422ade5640
schroe0690 <schroedinger.gs@gmail.com>
2025-05-08 09:59:40 +0000

    Update nvchad config 2025-05-08 09:59:40

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,9 @@
+-- 読み込むlspの設定ファイル
+
+-- デフォルト設定を適用
 require("nvchad.configs.lspconfig").defaults()
 
+-- 読み込むlspの一覧
 local servers = { "html", "cssls", "pyright"}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
