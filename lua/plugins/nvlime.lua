91804622568d8528ac8e0fcef60269a9918d10ca
schroe0690 <schroedinger.gs@gmail.com>
2025-05-08 23:46:55 +0000

    Update nvchad config 2025-05-08 23:46:55

---
+++ lua/plugins/nvlime.lua
@@ -0,0 +1,16 @@
+return {
+  "monkoose/nvlime",
+  -- ft = "lisp", -- lispファイルを開いた際に有効化
+  dependencies = {
+    "monkoose/parsley"
+  },
+  config = function()
+    -- Swankサーバーへの自動接続設定（任意）
+    vim.g.nvlime_auto_connect = {
+      host = "127.0.0.1",
+      port = 4005,
+    }
+    -- 必要に応じて追加設定
+  end,
+}
+  
