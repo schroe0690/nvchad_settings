b5c2595e5741bb5c594b2f57b5defd7f1e4b44eb
schroe0690 <schroedinger.gs@gmail.com>
2025-05-10 10:51:20 +0000

    Update nvchad config 2025-05-10 10:51:20

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,9 +1,25 @@
--- 読み込むlspの設定ファイル
+-- ~/.config/nvim/lua/custom/plugins/lspconfig.lua
 
--- デフォルト設定を適用
-require("nvchad.configs.lspconfig").defaults()
+-- NvChadのデフォルトLSP設定を適用
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+local lspconfig = require "lspconfig"
 
--- 読み込むlspの一覧
-local servers = { "html", "cssls", "pyright"}
-vim.lsp.enable(servers)
+-- LSPサーバー一覧
+local servers = { "html", "cssls", "pyright" } -- alive_lspは個別に設定するため、ここからは削除
 
+-- alive-lsp（Common Lisp用LSP）の個別セットアップ
+lspconfig.alive_lsp.setup {
+  cmd = {
+    "sbcl",
+    "--load", "~/quicklisp/setup.lisp",
+    "--eval", "(ql:quickload :alive-lsp)",
+    "--eval", "(alive/server:start)"
+  },
+  filetypes = { "lisp", "lsp", "commonlisp" },
+  root_dir = lspconfig.util.root_pattern(".git", "."),
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
